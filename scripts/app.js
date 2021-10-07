8f98a1d99bceafb1f2bf83769b3fdce82f10d897
Benjamin <b.girardpro@gmail.com>
2021-10-07 16:17:32 +0200

    Add date and time in mobile version

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,17 @@
 const dateDisplay = document.getElementById('dateDisplay')
+const displayDate = document.getElementById('displayDate')
+const displayTime = document.getElementById('displayTime')
 
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
 function clockTick () {
     const date = new Date()
-    dateDisplay.innerHTML = `Nous sommes le ${date.toLocaleDateString(undefined, options)} - ${date.toLocaleTimeString('fr-FR')}`
+    if (window.matchMedia("(min-width: 600px)").matches) {
+        dateDisplay.innerHTML = `Nous sommes le ${date.toLocaleDateString(undefined, options)} - ${date.toLocaleTimeString('fr-FR')}`
+    } else {
+        displayDate.innerHTML = `${date.toLocaleDateString()}`
+        displayTime.innerHTML = `${date.toLocaleTimeString('fr-FR')}`
+    }
 }
 
 setInterval(clockTick, 1000)
